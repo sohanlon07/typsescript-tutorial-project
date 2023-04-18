3502c1012a66b7af36a95d73d3c300aa3596062c
shaneohanlon-77276 <shane.ohanlon@synechron.com>
2023-04-18 17:17:50 +0100

    objects

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-function add(n1, n2) {
-    if (typeof n1 !== 'number' && typeof n2 !== 'number') {
-        throw new Error('NaN');
-    }
-    return n1 + n2;
-}
-var number1 = 5;
-var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var person = {
+    name: 'Shane',
+    age: 30
+};
+console.log(person.name);
