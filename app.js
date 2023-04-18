2fa6f9a977f3de4d512dcedd2afc8d5913342424
shaneohanlon-77276 <shane.ohanlon@synechron.com>
2023-04-18 17:32:32 +0100

    Tuples

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var person = {
     name: 'Shane',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
+person.role.push('admin');
+//person.role[1] = 10;
+var favouriteActivities;
+favouriteActivities = ['Sports'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -1,6 +1,23 @@
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]
+} = {
     name: 'Shane',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
 
-console.log(person.name)
+person.role.push('admin');
+//person.role[1] = 10;
+
+let favouriteActivities: string[];
+favouriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
